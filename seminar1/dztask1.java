2c4b834cd8b829dd6aad93145ed1d9d6db90973b
nobilitariy <134000822+nobilitariy@users.noreply.github.com>
2024-08-15 12:54:18 +0000

    1

--- seminar1/dztask1.java
+++ seminar1/dztask1.java
@@ -5,33 +5,38 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class dztask1 {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int chiclo = scanner.nextInt();
-        ArrayList list = Number(chiclo);
+        ArrayList<Integer> list = generateRandomNumbers(chiclo);
         System.out.println(list);
-        System.out.println(delNumber(list));
-
+        System.out.println(countDivisors(list));
     }
-    public static ArrayList<Integer> Number(int number){
-        ArrayList list = new ArrayList<>();
+
+    public static ArrayList<Integer> generateRandomNumbers(int number) {
+        ArrayList<Integer> list = new ArrayList<>();
         Random random = new Random();
         for (int i = 0; i < number; i++) {
-            list.add(random.nextInt(1000000));
+            list.add(random.nextInt(1000000) + 1); // избегаем 0
         }
         return list;
     }
-    public static ArrayList<Integer> delNumber(ArrayList<Integer> list){
-        ArrayList list2 = new ArrayList<>();
+
+    public static ArrayList<Integer> countDivisors(ArrayList<Integer> list) {
+        ArrayList<Integer> divisorCounts = new ArrayList<>();
         for (Integer object : list) {
             int count = 0;
-            for(int i = 2; i < object; i++){
-                if(object % i == 0){
+            for (int i = 1; i * i <= object; i++) { // начинаем с 1, чтобы включить все делители
+                if (object % i == 0) {
                     count++;
+                    if(i * i != object){
+                        count++;
+                    }
                 }
             }
-            list2.add(count);
+            divisorCounts.add(count);
         }
-        return list2;
+        return divisorCounts;
     }
+
 }
